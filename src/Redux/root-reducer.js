19658990a0a589e9d4fa6cce0e46e8eda87f4c89
Parimal Radadiya <72181399+ParimalRadadiya@users.noreply.github.com>
2022-08-09 14:33:14 +0530

    Delete src directory

--- src/Redux/root-reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import todosReducer from "./reducer";
-
-const rootReducer = combineReducers({
-    todos: todosReducer,
-});
-
-export default rootReducer;
